8632f8b03184236d0332cb31eda056fcc337c28d
chenxiaodong <chenxiaodong@xiaomi.com>
2016-10-17 13:17:43 +0800

    refactor dir

--- install.sh
+++ install.sh
@@ -2,13 +2,49 @@
 
 # inspired by https://packagecontrol.io/docs/syncing#git
 
-# 准备tpl的工具
-./subl-build-tpl-plugin/install.sh
+sublime=`dirname $0`
+#sublime=`cd $sublime;pwd;`
+#echo $sublime
+pkgs=''
+
+for pkg in `find $sublime -not -path '*/.*' -maxdepth 1 -type d | grep -v '^\.$'`
+do
+    pkg=`cd $pkg;pwd;`
+    pkgs="$pkgs $pkg"
+done
 
 cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
-mv User User_back
-mv subl-build-tpl-plugin subl-build-tpl-plugin_back 
-ln -s ~/.sublime User
-ln -s ~/.sublime/subl-build-tpl-plugin subl-build-tpl-plugin 
+
+for pkg in $pkgs
+do
+    basename=${pkg##*/}
+    rm -rf $basename
+    ln -s $pkg $basename
+done
+
+# 准备tpl的工具
+#./subl-build-tpl-plugin/install.sh
+
+# find . -not -path '*/\.*' -maxdepth 1 -type d
+# User
+#cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+#mv User User_back
+#ln -s ~/.config/sublime/User User
+
+#for pkg in `find $sublime -not -path '*/.*' -maxdepth 1 -type d | grep -v '^\.$'`
+#do
+    #pkg=`cd $pkg;pwd;`
+    #echo $pkg
+#done
+# packages
+
+#rm -rf subl-build-tpl-plugin
+#ln -s ~/.config/sublime/packages/subl-build-tpl-plugin subl-build-tpl-plugin 
+
+#rm -rf termX
+#ln -s ~/.config/sublime/packages/termX termX
+
+#rm -rf viper
+#ln -s ~/.config/sublime/packages/viper viper
 
 
