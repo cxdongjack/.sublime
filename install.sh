665cb9ccc46c5f1e0bd883680f543b172452b0e0
chenxiaodong <chenxiaodong@xiaomi.com>
2016-10-17 14:41:21 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 # inspired by https://packagecontrol.io/docs/syncing#git
 
 sublime=`dirname $0`
-#sublime=`cd $sublime;pwd;`
-#echo $sublime
+sublime_abs=`cd $sublime;pwd;`
 pkgs=''
 
 for pkg in `find $sublime -not -path '*/.*' -maxdepth 1 -type d | grep -v '^\.$'`
@@ -13,7 +12,16 @@ do
     pkgs="$pkgs $pkg"
 done
 
-cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
+# 进入Sublime目录
+cd ~/Library/Application\ Support/Sublime\ Text\ 3/
+
+# 准备包管理器
+cd Installed\ Packages/
+
+#! [[ -f 'Package Control.sublime-package' ]] && cp $sublime_abs/Package\ Control.sublime-package .
+
+# 准备配置文件
+cd ../Packages/
 
 for pkg in $pkgs
 do
@@ -21,30 +29,3 @@ do
     rm -rf $basename
     ln -s $pkg $basename
 done
-
-# 准备tpl的工具
-#./subl-build-tpl-plugin/install.sh
-
-# find . -not -path '*/\.*' -maxdepth 1 -type d
-# User
-#cd ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/
-#mv User User_back
-#ln -s ~/.config/sublime/User User
-
-#for pkg in `find $sublime -not -path '*/.*' -maxdepth 1 -type d | grep -v '^\.$'`
-#do
-    #pkg=`cd $pkg;pwd;`
-    #echo $pkg
-#done
-# packages
-
-#rm -rf subl-build-tpl-plugin
-#ln -s ~/.config/sublime/packages/subl-build-tpl-plugin subl-build-tpl-plugin 
-
-#rm -rf termX
-#ln -s ~/.config/sublime/packages/termX termX
-
-#rm -rf viper
-#ln -s ~/.config/sublime/packages/viper viper
-
-
